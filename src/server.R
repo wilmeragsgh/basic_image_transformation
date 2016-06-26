0c7efda136cdce89a1bf248b26a03b32628b3c2c
wilmeragsgh <wilmeragsgm@gmail.com>
2016-06-25 23:18:25 -0400

    adding some style

--- src/server.R
+++ src/server.R
@@ -5,10 +5,28 @@ shinyServer(function(input, output) {
       
       files <- reactive({
         files <- input$files
+        arc <- file(files$datapath,'rb')
         files$datapath <- gsub("\\\\", "/", files$datapath)
+        headr <- c()
+        headr <- cbind(headr,firm = readBin(arc,what = integer(),size = 2))
+        headr <- cbind(headr,filesize = readBin(arc,what = integer(),size = 4))
+        readBin(arc,what = integer(),size = 4) #skipping reserved
+        readBin(arc,what = integer(),size = 4) #skipping offset 
+        readBin(arc,what = integer(),size = 4) #skipping headersize
+        headr <- cbind(headr,width = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,height = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,colourPlanes = readBin(arc,what = integer(),size = 2))
+        headr <- cbind(headr,bitsPerPixel = readBin(arc,what = integer(),size = 2))
+        headr <- cbind(headr,compressionType = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,imageSize = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,xResolution = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,yResolution = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,nColors = readBin(arc,what = integer(),size = 4))
+        headr <- cbind(headr,impColors = readBin(arc,what = integer(),size = 4))
+        close(arc)
+        output$hdr <- renderTable(as.data.frame(headr))
         files
       })
-      
       
       output$images <- renderUI({
         if(is.null(input$files)) return(NULL)
--- src/ui.R
+++ src/ui.R
@@ -1,14 +1,24 @@
 ui <- shinyUI(fluidPage(
-  titlePanel("Uploading Files"),
+    tags$head(tags$style(HTML("
+    .shiny-text-output {
+      background-color:#fff;
+    }
+  "))),
+    
+    h1("BIP", span("Basic Image Processing", style = "font-weight: 300"), 
+       style = "font-family: 'Source Sans Pro';
+        color: #fff; text-align: center;
+        background-image: url('texturebg.png');
+        padding: 20px"),
+    br(),
   sidebarLayout(
     sidebarPanel(
       fileInput(inputId = 'files', 
                 label = 'Select an Image',
-                multiple = TRUE,
-                accept=c('image/png', 'image/jpeg'))
+                accept=c('image/bmp', 'image/bmp','.bmp'))
     ),
     mainPanel(
-      tableOutput('files'),
+        tableOutput('hdr'),
       uiOutput('images')
     )
   )
